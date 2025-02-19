63c394383ebf5ef3741017fed7ddf47d2ca41fae
gsandrof66 <gsandrof66@gmail.com>
2025-02-19 21:17:33 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Accessible_example
+
+## Overview
+This project highlights the importance of making your code accessible to all users. It is important to make sure that your code is accessible to all users, including those with disabilities. This project provides an example of how to make your R shiny app accessible to all users.
+
+## Installation
+To install the necessary packages, you can use the following commands in R:
+
+```r
+install.packages("shiny")
+install.packages("bslib")
+install.packages("data.table")
+install.packages("dplyr")
+install.packages("dtplyr")
+install.packages("sf")
+install.packages("plotly")
+```
+
+## Example
+URL: https://rpaddingtonguy.shinyapps.io/accessible_example/
---
+++ app.R
@@ -0,0 +1,80 @@
+library(shiny) # 1.10.0
+library(bslib) # 0.9.0
+library(data.table) # 1.16.4
+library(dplyr) # 1.1.4
+library(dtplyr) # 1.3.1
+library(sf) # 1.0.19
+library(plotly) # 4.10.4
+
+shapefile <- fread("./hb_codes.csv") |> 
+  arrange(HBName)
+
+hb_choices <- setNames(shapefile$HB, shapefile$HBName)
+
+final <- fread("./total_bed.csv") |> 
+  arrange(Quarter)
+
+bootswatch_themes <- c("flatly", "vapor")
+
+ui <- tags$html(
+  lang = "en",
+  fluidPage(
+    theme = bs_theme(bootswatch = "vapor"),
+    title = "Main Page",
+    h1("Shiny website", style = "text-align: center;"),
+    titlePanel("Choose your options"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("theme", "Choose a theme:", 
+                    choices = bootswatch_themes,
+                    selectize = FALSE,
+                    selected = "vapor"),
+        
+        selectInput("board_name", "Choose a board name:",
+                    choices = hb_choices,
+                    multiple = TRUE),
+        uiOutput("selected_items")
+      ),
+      mainPanel(
+        plotlyOutput("plot")
+      )
+    )
+  )  
+)
+
+server <- function(input, output, session){
+  observe({
+    session$setCurrentTheme(
+      bs_theme(bootswatch = input$theme)
+    )
+  })
+  
+  output$selected_items <- renderUI({
+    if(is.null(input$board_name)) return(NULL)
+    list_names <- shapefile[HB %in% input$board_name]$HBName
+    tags$ul(
+      lapply(list_names, function(board) {
+        tags$div(HTML(paste("<label style='display:none;'>", board, "</label>")))
+      })
+    )
+  })
+  
+  output$plot <- renderPlotly({
+    p <- plot_ly(if(!is.null(input$board_name)){final[HB %in% input$board_name]}else{final}, 
+                 x = ~Quarter, y = ~percFull, color = ~HBName, 
+                 type = 'scatter', mode = 'lines+markers',
+                 text = ~paste(Quarter, '<br>', HBName, '<br>', percFull, '%'),
+                 hoverinfo = 'text') |> 
+      layout(title = "Quarter Series Plot",
+             xaxis = list(title = "Quarter"),
+             yaxis = list(title = "Percentage full beds hosp"),
+             plot_bgcolor = 'black', 
+             paper_bgcolor = 'black',
+             font = list(color = 'white'))
+
+    p <- htmlwidgets::onRender(p,"function(el, x) {el.setAttribute('aria-label', 'This plot shows time series full bed occupancy per quarter');}")
+    p
+  })
+}
+
+shinyApp(ui, server)
