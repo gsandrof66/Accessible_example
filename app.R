7780fdd0792916da3531312859ae9fc729caf7ac
Gatz FO <gsandrof66@gmail.com>
2025-05-16 13:15:37 +0000

    dark mode

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny) # 1.10.0
 library(bslib) # 0.9.0
+library(thematic)
 library(data.table) # 1.16.4
 library(dplyr) # 1.1.4
 library(dtplyr) # 1.3.1
@@ -11,102 +12,96 @@ source("./model/Load_data.R")
 data_base_list <- data_base()
 
 base_alt <- "This plot shows time series full bed occupancy per quarter"
+theme_color = list(bgcolor = "white", fgcolor = "black")
 
-ui <- fluidPage(
-  # theme = bs_theme(bootswatch = "flatly"), 
+ui <- page_navbar(
+  theme = bs_theme(version = 5, bootswatch = NULL, bg = theme_color$bgcolor, fg = theme_color$fgcolor), 
   
-  # Title & Theme Selector
-  titlePanel(textOutput("dynamicTitle")),
-  selectInput("theme", "Select Theme:", choices = c("vapor", "flatly"), selected = "flatly"),
-  
-  # Navigation Bar
-  navbarPage("My App",
-             tabPanel("Home", fluidPage(h2("Welcome to Home"))),
-             tabPanel("Data", fluidPage(h2("Data Section"), tableOutput("data_table"))),
-             tabPanel("Maps", fluidPage(h2("Map review"))),
-             tabPanel("Settings", fluidPage(h2("App Settings")))
-  )
-  
-  # fluidPage(
-  #   theme = bs_theme(bootswatch = "vapor"),
-  #   title = "Main Page",
-  #   h1("Shiny website", style = "text-align: center;"),
-  #   titlePanel("Choose your options"),
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       selectInput("theme", "Choose a theme:", 
-  #                   choices = c("flatly", "vapor"),
-  #                   selectize = FALSE,
-  #                   selected = "vapor"),
-  #       
-  #       selectInput("board_name", "Choose a board name:",
-  #                   choices = data_base_list[["hb_choices"]],
-  #                   multiple = TRUE),
-  #       uiOutput("selected_items")
-  #     ),
-  #     mainPanel(
-  #       plotlyOutput("plot")
-  #     )
-  #   )
-  # )  
-  
+  nav_panel("Home", h2("Welcome to Home")),
+  nav_panel("Data", 
+            h2("Data Section"),
+            sidebarLayout(
+              sidebarPanel(
+                selectInput("board_name", "Choose a board name:",
+                            choices = data_base_list[["hb_choices"]],
+                            multiple = TRUE),
+                uiOutput("selected_items")
+              ),
+              mainPanel(
+                plotlyOutput("plot")
+              )
+            )
+            ),
+  nav_panel("Maps", h2("Map review")),
+  nav_panel("Settings", 
+            h2("App Settings"),
+            sidebarLayout(
+              sidebarPanel(h3("UI Theme")),
+              mainPanel(
+                materialSwitch(inputId = "dark_mode", label = "Dark Mode",
+                               status = "primary")
+                ))
+            )
 )
 
 server <- function(input, output, session){
-  # observe({
-  #   session$setCurrentTheme(
-  #     bs_theme(bootswatch = input$theme)
-  #   )
-  # })
+  thematic_shiny()
   
-  output$dynamicTitle <- renderText({
-    switch(input$navbar, 
-           "Home" = "Welcome to My App",
-           "Data" = "Data Analysis",
-           "Maps" = "Heat map",
-           "Settings" = "App Configuration",
-           "My App" # Default title
+  observe({
+    if(input$dark_mode){
+      theme_color = list(bgcolor = "black", fgcolor = "white")
+    }else{
+      theme_color = list(bgcolor = "white", fgcolor = "black")
+    }
+    new_theme <- bs_theme(version = 5, bootswatch = NULL, bg = theme_color$bgcolor, fg = theme_color$fgcolor)
+    session$setCurrentTheme(new_theme)
+  })
+  
+  output$selected_items <- renderUI({
+    if(is.null(input$board_name)) return(NULL)
+    list_names <- data_base_list[["shapefile"]][HB %in% input$board_name]$HBName
+    tags$ul(
+      lapply(list_names, function(board) {
+        tags$div(HTML(paste("<label style='display:none;'>", board, "</label>")))
+      })
     )
   })
   
-  # output$selected_items <- renderUI({
-  #   if(is.null(input$board_name)) return(NULL)
-  #   list_names <- data_base_list[["shapefile"]][HB %in% input$board_name]$HBName
-  #   tags$ul(
-  #     lapply(list_names, function(board) {
-  #       tags$div(HTML(paste("<label style='display:none;'>", board, "</label>")))
-  #     })
-  #   )
-  # })
-  # 
-  # output$plot <- renderPlotly({
-  #   p <- plot_ly(if(!is.null(input$board_name)){data_base_list[["final"]][HB %in% input$board_name]}else{data_base_list[["final"]]}, 
-  #                x = ~Quarter, y = ~percFull, color = ~HBName, 
-  #                type = 'scatter', mode = 'lines+markers',
-  #                text = ~paste(Quarter, '<br>', HBName, '<br>', percFull, '%'),
-  #                hoverinfo = 'text') |> 
-  #     layout(title = "Quarter Series Plot",
-  #            xaxis = list(title = "Quarter"),
-  #            yaxis = list(title = "Percentage full beds hosp"),
-  #            plot_bgcolor = 'black', 
-  #            paper_bgcolor = 'black',
-  #            font = list(color = 'white'))
-  #   
-  #   if(!is.null(input$board_name)){
-  #     list_names <- data_base_list[["shapefile"]][HB %in% input$board_name]$HBName
-  #     final_alt <- paste0(base_alt, " for ", paste(list_names, collapse = ", "))
-  #   }else{
-  #     final_alt <- paste0(base_alt, " for all the health boards")
-  #   }
-  #   
-  #   # Add dynamic alt text using htmlwidgets::onRender
-  #   p <- htmlwidgets::onRender(p, sprintf("
-  #     function(el, x) {
-  #       el.setAttribute('aria-label', '%s');
-  #     }
-  #   ", final_alt))
-  #   p
-  # })
+  output$plot <- renderPlotly({
+    if(input$dark_mode){
+      theme_color = list(bgcolor = "black", fgcolor = "white")
+    }else{
+      theme_color = list(bgcolor = "white", fgcolor = "black")
+    }
+    
+    p <- plot_ly(if(!is.null(input$board_name)){data_base_list[["final"]][HB %in% input$board_name]}else{data_base_list[["final"]]},
+                 x = ~Quarter, y = ~percFull, color = ~HBName,
+                 type = 'scatter', mode = 'lines+markers',
+                 text = ~paste(Quarter, '<br>', HBName, '<br>', percFull, '%'),
+                 hoverinfo = 'text') |>
+      layout(title = "Quarter Series Plot",
+             xaxis = list(title = "Quarter"),
+             yaxis = list(title = "Percentage full beds hosp"),
+             plot_bgcolor = theme_color$bgcolor,
+             paper_bgcolor = theme_color$bgcolor,
+             font = list(color = theme_color$fgcolor)
+             )
+
+    if(!is.null(input$board_name)){
+      list_names <- data_base_list[["shapefile"]][HB %in% input$board_name]$HBName
+      final_alt <- paste0(base_alt, " for ", paste(list_names, collapse = ", "))
+    }else{
+      final_alt <- paste0(base_alt, " for all the health boards")
+    }
+
+    # Add dynamic alt text using htmlwidgets::onRender
+    p <- htmlwidgets::onRender(p, sprintf("
+      function(el, x) {
+        el.setAttribute('aria-label', '%s');
+      }
+    ", final_alt))
+    p
+  })
 }
 
 shinyApp(ui, server)
